6cea2c741f94af832f585fe1b0b15db4ee147d51
Chekcheev_Alexandr <chekcheev.alex@gmail.com>
2022-06-25 16:08:38 +0300

    first commit

---
+++ vue_code.js
@@ -0,0 +1,35 @@
+let app = new Vue({
+    el: '#karting',
+    data: {
+        menu: ['Цены/прокат', 'Акции', 'Аренда', 'Трасса', 'Соревнования', 'Школа', 'Гоночная команда', 'Продажа картингов'],
+        logo: 'logo.png',
+        main: 'main.png',
+        buttonGetStarted: 'button_get_started.png',
+        f1: 'feature1.png',
+        f2: 'feature2.png',
+        f3: 'feature3.png',
+        f4: 'feature4.png',
+        f5: 'feature5.png',
+        f6: 'feature6.png',
+        contacts: 'contacts.png',
+        largeContacts: 'large_contacts.png',
+        recallButton: 'recall_button.png',
+        insta: 'insta.png',
+        youtube: 'youtube.png',
+        twitter: 'twitter.png',
+        facebook: 'facebook.png',
+        isContactsActive: false
+    },
+    methods: {
+        mouseOverContacts: function (event) {
+            if (event) {
+                this.isContactsActive = true;
+            }
+        },
+        mouseOutContacts: function (event) {
+            if (event) {
+                this.isContactsActive = false;
+            }
+        }
+    }
+})
